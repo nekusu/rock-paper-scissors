1f2650547d882725cd99703c19d400fb376f206d
nekusu <nosoynahuel11@gmail.com>
2021-07-07 02:56:44 -0300

    Add score and announce the winner

--- js/rockPaperScissors.js
+++ js/rockPaperScissors.js
@@ -1,9 +1,21 @@
 const options = ['rock', 'paper', 'scissors']
+const rps = document.querySelectorAll('.rps')
+const computerSelectionImg = document.querySelector('#computer-selection')
+const score = document.querySelector('#score')
 const results = document.querySelector('#results')
+const buttons = document.querySelectorAll('button.rps')
+const restartButton = document.querySelector('#restart-button')
+let playerScore = 0
+let computerScore = 0
 
 function computerPlay() {
 	const randomIndex = Math.floor(Math.random() * options.length)
+	computerSelectionImg.src = `svg/${options[randomIndex]}.svg`
 	return options[randomIndex]
+}
+
+function restartButtonsScale() {
+	buttons.forEach(button => button.classList.remove('selected'))
 }
 
 function showResults(result, color) {
@@ -11,20 +23,54 @@ function showResults(result, color) {
 	results.style.color = color
 }
 
+function updateScore() {
+	score.textContent = `${playerScore} - ${computerScore}`
+}
+
+function toggleClasses() {
+	rps.forEach(rps => rps.classList.toggle('hidden'))
+	score.classList.toggle('big')
+	results.classList.toggle('big')
+	restartButton.classList.toggle('hidden')
+}
+
+function checkWinner() {
+	if (playerScore >= 5 || computerScore >= 5) {
+		toggleClasses()
+		restartButtonsScale()
+	}
+}
+
 function playRound(e) {
 	const playerSelection = e.target.parentNode.getAttribute('id')
 	const computerSelection = computerPlay()
 	const playerSelectionIndex = options.indexOf(playerSelection)
 	const computerSelectionIndex = options.indexOf(computerSelection)
+	restartButtonsScale()
+	e.target.parentNode.classList.toggle('selected')
 
 	if (playerSelectionIndex == computerSelectionIndex + (computerSelectionIndex == 2 ? -2 : 1)) {
-		showResults('You Win!', 'green')
+		playerScore++
+		showResults('You Win!', 'lightgreen')
 	} else if (playerSelectionIndex == computerSelectionIndex) {
-		showResults('Draw!', 'blue')
+		showResults('Draw!', 'lightblue')
 	} else {
-		showResults('You Lose!', 'red')
+		computerScore++
+		showResults('You Lose!', 'pink')
 	}
+
+	updateScore()
+	checkWinner()
 }
 
-const buttons = document.querySelectorAll('button')
+function restartGame() {
+	playerScore = 0
+	computerScore = 0
+	computerSelectionImg.src = ''
+	updateScore()
+	showResults('Select an option', 'rgb(170, 170, 170)')
+	toggleClasses()
+}
+
 buttons.forEach(button => button.addEventListener('click', playRound))
+restartButton.addEventListener('click', restartGame)
